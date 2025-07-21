a1fcf0202d4f0773952e5d97456ad29614033f5a
Bhumika <bhumikat2910@gmail.com>
2025-07-21 19:07:20 +0530

    Fixed import bug

--- app/api/summarize/route.ts
+++ app/api/summarize/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession, Session } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../../../lib/auth";
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
